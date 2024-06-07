8a9b3f5f1c29d85db40f918d66b696f9ddf41e30
Fran Moreno <fran.siner@gmail.com>
2024-06-07 12:22:25 +0200

    Completed films test

--- __tests__/films.test.tsx
+++ __tests__/films.test.tsx
@@ -15,17 +15,17 @@ test('renders Films component', () => {
     {
       id: 2,
       title: 'Film 2',
-      releaseDate: '1977-05-25',
+      releaseDate: '1978-05-25',
       planetConnection: {
-        planets: [{ surfaceWater: 0 }, { surfaceWater: 50 }],
+        planets: [{ surfaceWater: 50 }, { surfaceWater: 50 }],
       },
     },
     {
       id: 3,
       title: 'Film 3',
-      releaseDate: '1977-05-25',
+      releaseDate: '1979-05-25',
       planetConnection: {
-        planets: [{ surfaceWater: 0 }, { surfaceWater: 50 }],
+        planets: [{ surfaceWater: 0 }, { surfaceWater: 0 }],
       },
     },
   ]
@@ -33,6 +33,9 @@ test('renders Films component', () => {
   const { getByText } = render(<Films films={films} />)
 
   expect(getByText('Film 1')).toBeInTheDocument()
+  expect(getByText('Release Date: 1977-05-25')).toBeInTheDocument()
+  expect(getByText('1 planet without water')).toBeInTheDocument()
+
   act(() => {
     const previousButton = getByText('Previous')
     fireEvent.click(previousButton)
@@ -44,12 +47,16 @@ test('renders Films component', () => {
     fireEvent.click(nextButton)
   })
   expect(getByText('Film 2')).toBeInTheDocument()
+  expect(getByText('Release Date: 1978-05-25')).toBeInTheDocument()
+  expect(getByText('0 planet without water')).toBeInTheDocument()
 
   act(() => {
     const nextButton = getByText('Next')
     fireEvent.click(nextButton)
   })
   expect(getByText('Film 3')).toBeInTheDocument()
+  expect(getByText('Release Date: 1979-05-25')).toBeInTheDocument()
+  expect(getByText('2 planets without water')).toBeInTheDocument()
 
   act(() => {
     const nextButton = getByText('Next')
